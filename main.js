0b3c5d457f755974b0adeda860b5157c1b2daa96
Abdulla Shoymardonov <abdulla1998uz@gmail.com>
2023-03-13 23:56:03 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const refreshButton = document.querySelector(".refresh-btn");
+const maxPaletteBoxes= 32;
+const generatePalette = () => {
+	container.innerHTML= " ";
+	for (var i = 0; i < maxPaletteBoxes; i++) {
+	let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+	randomHex = ` #${randomHex.padStart(6, "0")}`.toUpperCase();
+
+	const color = document.createElement("li");
+	color.classList.add("color");
+	color.innerHTML = `<div class="rect-box" style = "background :${randomHex}"></div> 
+					   <span class="hex-value">${randomHex}</span>`;
+	color.addEventListener("click", () => copyColor(color, randomHex));				   
+	container.appendChild(color);
+	}
+}
+generatePalette();
+
+const copyColor= (element, hexVal) => {
+	const colorElement = element.querySelector('.hex-value');
+	navigator.clipboard.writeText(hexVal).then(() => {
+		colorElement.innerText = "Copied";
+
+		setTimeout(() => colorElement.innerText = hexVal, 1000);
+	}).catch(() =>  alert("Failed to copy"));
+}
+refreshButton.addEventListener("click", generatePalette);
+
